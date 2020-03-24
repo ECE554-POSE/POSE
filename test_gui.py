e4abe58650d95bac64da1d05b42bcdf6ae8301a6
Luisx09 <lrodrigue@ltu.edu>
2020-03-24 19:21:05 -0400

    Added simple GUI
    Need to make display much smoother and resolve shutdown lock

--- test_gui.py
+++ test_gui.py
@@ -1,16 +1,51 @@
 import tkinter as tk
+from functools import partial
+from jetcam.usb_camera import USBCamera
+from jetcam.utils import bgr8_to_jpeg
+from PIL import Image
+from PIL import ImageTk
 
-def game_start():
-    print("Game time started")
-    # Need to either start a Tkinter screen with the camera feed or use a matplotlib for it
-    
+class POSE_GUI:
+	def __init__(self):
+		self.WIDTH = 224
+		self.HEIGHT = 224
+		self.root = tk.Tk()
+		self.root.title('POSE')
+		self.camera = USBCamera(width=self.WIDTH, height=self.HEIGHT, capture_fps=30)
+		self.camera.running = True
+		self.start_button = tk.Button(self.root, text= 'Start Game', command=self.game_start)
+		self.start_button.pack(side=tk.TOP, padx=5, pady=5)
+		self.exit_button = tk.Button(self.root, text= 'Quit', command=self.exit_app)
+		self.exit_button.pack(side=tk.TOP, padx=5, pady=5)
+		self.lmain = tk.Label(self.root)
+		self.lmain.pack()
+		
+	def execute(self, change):
+		img = change['new']
+		# data = preprocess(image)
+		# cmap, paf = model_trt(data)
+		# cmap, paf = cmap.detach().cpu(), paf.detach().cpu()
+		# counts, objects, peaks = parse_objects(cmap, paf)#, cmap_threshold=0.15, link_threshold=0.15)
+		# draw_objects(image, counts, objects, peaks)
+		# imgdraw = bgr8_to_jpeg(image[:, ::-1, :])
+		img=Image.fromarray(img)
+		imgtk = ImageTk.PhotoImage(image=img)
+		self.lmain.imgtk = imgtk
+		self.lmain.configure(image=imgtk)
+
+
+	def game_start(self):
+		print("Game time started")
+		# Need to either start a Tkinter screen with the camera feed or use a matplotlib for it
+		self.camera.observe(self.execute, names='value')
+
+	def exit_app(self):
+		self.camera.unobserve_all()
+		self.camera.running = False
+		self.root.quit()
+
 
 
 if __name__ == '__main__':
-    root = tk.Tk()
-    root.title('POSE')
-    start_button = tk.Button(root, text= 'Start Game', command=game_start)
-    start_button.pack(side=tk.LEFT, padx=5, pady=5)
-    exit_button = tk.Button(root, text= 'Quit', command=root.quit)
-    exit_button.pack(side=tk.LEFT, padx=5, pady=5)
-    root.mainloop()
+	POSE = POSE_GUI()
+	POSE.root.mainloop()
