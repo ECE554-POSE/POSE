0755d3d860dc1b8026bd36da1770e2e0736c9f44
Zeratul120 <mdiederi@umich.edu>
2020-04-07 23:46:44 -0400

    This commit adds a drop down menu to the selection page and a timer to the posing phase. Currently both buttons on the pose phase are broken, but the main menu button will be fixed in the next commit

--- GUI/button_panel.py
+++ GUI/button_panel.py
@@ -7,8 +7,11 @@ import tkinter as Tk
 class ButtonPanel():
 	def __init__(self, root):
 		self.frame2 = Tk.Frame(root)
-		self.frame2.pack(side=Tk.RIGHT, fill=Tk.BOTH, expand=1)
 		self.button1 = Tk.Button(self.frame2, text="Option 1")
 		self.button1.pack(side="top", fill=Tk.BOTH)
 		self.button2 = Tk.Button(self.frame2, text="Option 2")
 		self.button2.pack(side="top", fill=Tk.BOTH)
+	
+	def pack(self):
+		self.frame2.pack(side=Tk.RIGHT, fill=Tk.BOTH, expand=1)
+		
